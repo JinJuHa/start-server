33516ea690918341491bc18c548894235f093246
LAPTOP-JJPVHET9\User <jooha321@gmail.com>
2022-09-11 18:24:06 +0900

    20220911

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,12 @@
+const home = (req, res)=>{
+    res.render('home/index.html')
+};
+
+const login = (req, res)=>{
+    res.render('home/login.html')
+};
+
+module.exports = {
+    home,
+    login
+}
