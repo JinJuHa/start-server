69d6d04dbd33839dfee7043a8e5ed23fd9e74e1e
LAPTOP-JJPVHET9\User <jooha321@gmail.com>
2022-09-12 21:23:43 +0900

    돔을 이용하여 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+//const id = document.querySelector('선택자')
+const id = document.querySelector('#id'),
+    pword = document.querySelector('#pw'),
+    loginb = document.querySelector('button');
+
+//console.log(id);
+
+loginb.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        pw : pw.value,
+    }
+    console.log(req);
+}
